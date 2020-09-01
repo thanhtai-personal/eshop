2987d269f4f9907019dc1f6a833ef70be33da0e5
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2020-09-01 21:32:58 +0700

    change config

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const options = {};
 
 // options.ip = '127.0.0.1';
 // options.port = parseInt(process.argv[2]);
-options.port = 3004;
+// options.port = 3004;
 // options.config = { name: 'Total.js' };
 // options.sleep = 3000;
 // options.inspector = 9229;
